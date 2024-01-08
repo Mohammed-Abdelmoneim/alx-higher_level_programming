6b9c1ce175daad2ff5c1cec4700106880bc2db62
root <root@Moneim-PC.localdomain>
2024-01-08 10:16:47 +0200

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,4 +2,6 @@
 ownDir = dir("/__pycache__/hidden_4.pyc")
 if __name__ == '__main__':
     filtered_dir = [name for name in ownDir if not name.startswith('__')]
-    print(filtered_dir)
+    for i in range(len(filtered_dir)):
+        print(filtered_dir[i])
+    
