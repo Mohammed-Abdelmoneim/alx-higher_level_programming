1960eee30a03205d1dec45c03f50e899dbf65d46
root <root@Moneim-PC.localdomain>
2024-01-08 10:53:09 +0200

    fix error tast 4 decmpile

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-ownDir = dir("/__pycache__/hidden_4.pyc")
+import decompile
+
+ownDir = dir(decompile)
 if __name__ == '__main__':
     filtered_dir = [name for name in ownDir if not name.startswith('__')]
     for i in range(len(filtered_dir)):
