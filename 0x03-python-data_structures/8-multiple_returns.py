d5392e813328334d754de87d887e7aea20a8db77
root <root@Moneim-PC.localdomain>
2024-01-09 18:15:31 +0200

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-      leng = len(sentence)
-      first_char = ""
-      if len(sentence) > 0:
-          first_char = sentence[0]
-      else:
-           first_char = "None"
-      return (leng, first_char)    
+    leng = len(sentence)
+    first_char = ""
+    if len(sentence) > 0:
+        first_char = sentence[0]
+    else:
+        first_char = "None"
+    return (leng, first_char)
