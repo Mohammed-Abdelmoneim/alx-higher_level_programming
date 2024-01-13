a36694d9e69bdabada147db71e6b8d68e1512888
root <root@Moneim-PC.localdomain>
2024-01-13 21:19:00 +0200

    7-update_dictionary.py

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    if a_dictionary and key and value:
+        a_dictionary.update({key: value})
+        return a_dictionary
+    else:
+        return a_dictionary
