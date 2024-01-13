a581ca4547a37dcc58e33e56fdafbceedfcbfcaa
root <root@Moneim-PC.localdomain>
2024-01-13 20:50:19 +0200

    4-only_diff_elements.py

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -3,5 +3,7 @@ def only_diff_elements(set_1, set_2):
     if set_1 and set_2:
         diff_1 = set_1.symmetric_difference(set_2)
         return diff_1
+    elif set_1 and not set_2:
+        return set_1
     else:
-        return list()
+        return set_2
