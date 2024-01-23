b0ceb05e6b3fa61ccdc83094f203dfcfd3e5f536
root <root@Moneim-PC.localdomain>
2024-01-23 06:54:16 +0200

    fix

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    #if my_list:
-        length = 0
-        for i in my_list:
-            length += 1
-        try:
-            if x <= length:
-                for i in range(x):
-                    print(my_list[i], end="")
-                print()
-                return x
-            else:
-                for i in my_list:
-                    print(i, end="")
-                print()
-                x = length
-                return x
-        except Exception:
-            print(Exception)
-    #else:
-     #   return 0
+    length = 0
+    for i in my_list:
+        length += 1
+    try:
+        if x <= length:
+            for i in range(x):
+                print(my_list[i], end="")
+            print()
+            return x
+        else:
+            for i in my_list:
+                print(i, end="")
+            print()
+            x = length
+            return x
+    except Exception:
+        print(Exception)
