73659f59df489685e173024d08bc8b49974ea478
root <root@Moneim-PC.localdomain>
2024-01-23 09:19:16 +0200

    4-list_division.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,20 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    new_list = list(filter(lambda a: type(a) == int, my_list))
-
-    length = 0
-    for i in new_list:
-        length += 1
-    try:
-        if x < length:
-            for i in range(x):
-                print("{:d}".format(new_list[i]), end="")
-            print()
-            return x
-        else:
-            for i in new_list:
-                print("{:d}".format(i), end="")
-            print()
-            return x
-    except Exception:
-        print(Exception)
+    count = 0
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except (ValueError, TypeError):
+            continue
+    print("")
+    return (count)
