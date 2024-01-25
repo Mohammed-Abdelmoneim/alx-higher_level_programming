208856563c96e263f8602dd0d2d34b9cc2c3287f
root <root@Moneim-PC.localdomain>
2024-01-25 21:21:24 +0200

    3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Simple class again about square."""
+
+
+class Square:
+    """
+    Continue to build the square class.
+    """
+
+    def __init__(self, size=0):
+        """
+        The constructor method.
+
+        Parameters:
+            size (int): the size of a square.
+        """
+        self.__size = size
+        self.check_size()
+
+    def check_size(self):
+        """
+        The function to check on the size.
+
+        Returns:
+            Square: raises an error on if statements.
+        """
+        if type(self.__size) != int:
+            raise TypeError("size must be an integer")
+        elif self.__size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """
+        Function that calc the area of a square.
+
+        Returns:
+            Square: the area of a square.
+        """
+        return self.__size * self.__size
