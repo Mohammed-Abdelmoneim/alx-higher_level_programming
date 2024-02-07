0daf7f358778945438483438fe52060f35cdd1a3
root <root@Moneim-PC.localdomain>
2024-02-08 01:32:00 +0200

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Simple class module."""
+
+
+class BaseGeometry:
+    """
+    Basic BaseGeometry empty class.
+    """
+    def area(self):
+        """
+        Raises an exception with message.
+        """
+        raise Exception("area() is not implemented")
