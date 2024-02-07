b3a1dd2fcbdca9fb7a9a39c5e0911da4c01af357
root <root@Moneim-PC.localdomain>
2024-02-07 21:58:09 +0200

    task 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Simple class module."""
+
+
+def is_same_class(obj, a_class):
+    """
+    Check if the obj is an instance of the specified class
+
+    Parameters:
+        obj: the obj to check.
+        a_class: the class itself
+    """
+    if isinstance(obj.__class__, a_class):
+        return True
+    else:
+        return False
