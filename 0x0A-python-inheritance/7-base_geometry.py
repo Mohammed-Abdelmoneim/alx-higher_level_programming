750171873fc6a53f2bde9ac50908725bae519f70
root <root@Moneim-PC.localdomain>
2024-02-08 02:07:37 +0200

    tasl7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Simple class module."""
+
+
+class BaseGeometry:
+    """
+    Basic BaseGeometry empty class.
+    """
+    def area(self):
+        """
+        Raises an exception with message.
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Check the value.
+        """
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        elif value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
