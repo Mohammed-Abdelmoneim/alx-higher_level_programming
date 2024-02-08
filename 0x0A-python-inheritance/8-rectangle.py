5cac59ecc57512bb9e08602033bc1fdd71b29a4b
root <root@Moneim-PC.localdomain>
2024-02-08 02:39:51 +0200

    tasl 8 fix

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,6 +1,25 @@
 #!/usr/bin/python3
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
 """Simple class module."""
+
+
+class BaseGeometry:
+    """
+    Basic BaseGeometry empty class.
+    """
+    def area(self):
+        """
+        Raises an exception with message.
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Check the value.
+        """
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        elif value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
 
 
 class Rectangle(BaseGeometry):
