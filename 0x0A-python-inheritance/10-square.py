8b34fa6ff16b57f86acdcd7919c624b80a852638
root <root@Moneim-PC.localdomain>
2024-02-13 10:35:28 +0200

    task 10:

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Simple class module."""
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+    """
+    Basic Square inherits from Rectangle.
+    """
+    def __init__(self, size):
+        self.__size = size
+        self.integer_validator("size", self.__size)
+    
+    def area(self):
+        """
+        returns the area of the square
+        """
+        return self.__size * self.__size
+        
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__size, self.__size)
