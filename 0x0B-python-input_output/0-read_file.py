0fadac9cdc84b055c7f236593c9f4f773926e19d
root <root@Moneim-PC.localdomain>
2024-02-17 14:47:42 +0200

    first

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     """Read a file into stdout."""
-    with open(filename) as f:
-        print(f.read())
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
