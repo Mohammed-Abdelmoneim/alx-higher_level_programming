fb34ff8bf93054951ad61d8dc96c9f2adedc182b
root <root@Moneim-PC.localdomain>
2024-02-18 09:29:21 +0200

    fix task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 """Adds all arguments to a Python list, and then save them to a file"""
+import sys
 
 
 if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+    save_to = __import__('5-save_to_json_file').save_to_json_file
+    load_from = __import__('6-load_from_json_file').load_from_json_file
 
-    with open("add_item.json" "w") as f:
-        data = load_from_json_file(f)
-        save_to_json_file(data, "add_item.json")
+    try:
+        items = load_from("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to(items, "add_item.json")
