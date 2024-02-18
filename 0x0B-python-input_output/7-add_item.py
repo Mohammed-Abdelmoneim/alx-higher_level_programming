6c85ad30064fd778af3742cabb214c7cc02a84e9
root <root@Moneim-PC.localdomain>
2024-02-18 09:21:58 +0200

    task 7 fix

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""Simple module."""
-import json
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+"""Adds all arguments to a Python list, and then save them to a file"""
 
 
-with open("add_item.json" "w") as f:
-    data = load_from_json_file(f)
-    save_to_json_file(data, "add_item.json")
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+    with open("add_item.json" "w") as f:
+        data = load_from_json_file(f)
+        save_to_json_file(data, "add_item.json")
