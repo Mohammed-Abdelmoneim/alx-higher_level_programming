cd5b964ae4b482995261adc3e0b23a9fccae3c87
root <root@DESKTOP-U4P7FN4.localdomain>
2024-07-01 17:34:19 +0300

    second biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -8,12 +8,12 @@ if (!args[0] || args[0] === 1) {
   let sec = 0;
   for (let i = 0; i < args.length; i++) {
     if (args[i] > num) {
-      num = args[i];
+      num = Number(args[i]);
     }
   }
   for (let i = 0; i < args.length; i++) {
     if (args[i] > sec && args[i] < num) {
-      sec = args[i];
+      sec = Number(args[i]);
     }
   }
   console.log(sec);
